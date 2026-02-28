e4f8b88d65e99a5ac9368d1b523029d74c70f1b2
Bruno Rocha Dulcetti <brunodulcetti@gmail.com>
2026-02-27 23:18:48 -0300

    feat: add container on layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
 import type { ReactNode } from 'react'
 import { SiteNavbar } from '@/components/header-navbar'
 
+import '@/styles/styles.scss'
+import styles from '@/styles/Grid.module.scss';
+
 interface RootLayoutProps {
   children: ReactNode
 }
 
-import '@/styles/styles.scss'
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="pt-BR">
       <body>
         <SiteNavbar logoSrc="/theme/marca-pdb@1x.png" />
-        {children}
+        <main className={styles['container-fluid']}>
+          {children}
+        </main>
       </body>
     </html>
   )
