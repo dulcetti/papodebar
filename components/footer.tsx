04af95a3375e7fedb8e32090fcb5a233e7df94ca
Bruno Rocha Dulcetti <brunodulcetti@gmail.com>
2026-03-28 10:37:07 -0300

    fix: update social links

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,7 +14,7 @@ export const SOCIAL_LINKS: SocialLink[] = [
 	{
 		id: 'facebook',
 		label: 'Facebook',
-		href: 'https://facebook.com',
+		href: 'https://facebook.com/papodebar',
 		color: '#3b5998',
 		iconPath:
 			'M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z',
@@ -22,7 +22,7 @@ export const SOCIAL_LINKS: SocialLink[] = [
 	{
 		id: 'twitter',
 		label: 'Twitter / X',
-		href: 'https://twitter.com',
+		href: 'https://twitter.com/papodebar_',
 		color: '#1da1f2',
 		iconPath:
 			'M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z',
@@ -30,7 +30,7 @@ export const SOCIAL_LINKS: SocialLink[] = [
 	{
 		id: 'youtube',
 		label: 'YouTube',
-		href: 'https://youtube.com',
+		href: 'https://youtube.com/papodebarvideos',
 		color: '#ff0000',
 		iconPath:
 			'M22.54 6.42a2.78 2.78 0 0 0-1.95-1.96C18.88 4 12 4 12 4s-6.88 0-8.59.46a2.78 2.78 0 0 0-1.95 1.96A29 29 0 0 0 1 12a29 29 0 0 0 .46 5.58A2.78 2.78 0 0 0 3.41 19.6C5.12 20 12 20 12 20s6.88 0 8.59-.46a2.78 2.78 0 0 0 1.95-1.95A29 29 0 0 0 23 12a29 29 0 0 0-.46-5.58zM9.75 15.02V8.98L15.5 12l-5.75 3.02z',
@@ -46,7 +46,7 @@ export const SOCIAL_LINKS: SocialLink[] = [
 	{
 		id: 'pinterest',
 		label: 'Pinterest',
-		href: 'https://pinterest.com',
+		href: 'https://pinterest.com/papodebar',
 		color: '#e60023',
 		iconPath:
 			'M12 2C6.48 2 2 6.48 2 12c0 4.24 2.65 7.86 6.39 9.29-.09-.78-.17-1.98.03-2.83.19-.77 1.27-5.38 1.27-5.38s-.32-.65-.32-1.61c0-1.51.88-2.64 1.97-2.64.93 0 1.38.7 1.38 1.54 0 .94-.6 2.34-.91 3.64-.26 1.09.54 1.97 1.6 1.97 1.92 0 3.4-2.02 3.4-4.95 0-2.59-1.86-4.4-4.52-4.4-3.08 0-4.89 2.31-4.89 4.7 0 .93.36 1.93.8 2.47.09.11.1.2.07.31-.08.34-.26 1.09-.3 1.24-.05.2-.16.24-.37.14-1.39-.65-2.26-2.68-2.26-4.32 0-3.51 2.55-6.74 7.36-6.74 3.86 0 6.86 2.75 6.86 6.42 0 3.83-2.41 6.91-5.76 6.91-1.13 0-2.19-.59-2.55-1.28l-.69 2.59c-.25.97-.93 2.18-1.39 2.92C10.72 21.94 11.35 22 12 22c5.52 0 10-4.48 10-10S17.52 2 12 2z',
@@ -54,7 +54,7 @@ export const SOCIAL_LINKS: SocialLink[] = [
 	{
 		id: 'instagram',
 		label: 'Instagram',
-		href: 'https://instagram.com',
+		href: 'https://instagram.com/papodebar',
 		color: '#c13584',
 		iconPath:
 			'M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37zM17.5 6.5h.01M7.5 2h9A5.5 5.5 0 0 1 22 7.5v9A5.5 5.5 0 0 1 16.5 22h-9A5.5 5.5 0 0 1 2 16.5v-9A5.5 5.5 0 0 1 7.5 2z',
@@ -201,5 +201,5 @@ export function Footer({
 				<address dangerouslySetInnerHTML={{ __html: copyrightText }}></address>
 			</div>
 		</footer>
-	)
+	);
 }
