44909a2113e81bb103af33033a28dc5e4ff3a9a5
Bruno Rocha Dulcetti <brunodulcetti@gmail.com>
2026-03-30 22:16:56 -0300

    feat: add \n to sanitizeXml

--- libs/sanitize.ts
+++ libs/sanitize.ts
@@ -1,6 +1,6 @@
 export function sanitizeXml(str: string) {
   return str.replace(
-    /[\u0000-\u0008\u000B\u000C\u000E-\u001F\u007F]/g,
+    /[\u0000-\u0008\u000B\u000C\u000E-\u001F\u007F/\n]/g,
     ''
   );
 }
