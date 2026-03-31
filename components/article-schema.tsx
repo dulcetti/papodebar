f8b7ada131b748f1dd6af634de16bd2a48613643
Bruno Rocha Dulcetti <brunodulcetti@gmail.com>
2026-03-30 22:20:59 -0300

    feat: add initial article schema

---
+++ components/article-schema.tsx
@@ -0,0 +1,48 @@
+import { Post } from "@/libs/posts";
+import { sanitizeXml } from "@/libs/sanitize";
+
+type Props = {
+  data: Post;
+}
+
+export function ArticleSchema({ data }: Props) {
+  const ldJson = {
+    "@context": "https://schema.org",
+    "@type": "Article",
+    "headline": data.title,
+    "description": sanitizeXml(data.content).slice(0, 200) + '...',
+    "image": [
+      data.coverImage
+    ],
+    "author": {
+      "@type": "Person",
+      "name": "Seu Nome"
+    },
+    "publisher": {
+      "@type": "Organization",
+      "name": "Papo de Bar",
+      "logo": {
+        "@type": "ImageObject",
+        "url": "https://www.papodebar.com/images/marca-pdb@1x.png"
+      }
+    },
+    "datePublished": data.date,
+    "dateModified": data.date,
+    "mainEntityOfPage": {
+      "@type": "WebPage",
+      "@id": `https://www.papodebar.com/${data.slug}`
+    },
+    "keywords": data.tags?.join(", ") || "",
+    "articleSection": data.categories.join(", "),
+    "wordCount": "1200"
+  }
+  console.info(data);
+  return (
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(ldJson).replace(/</g, '\\u003c')
+      }}
+    />
+  )
+}
