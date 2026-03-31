b518a4f98e87e0c650f44b8b344d4e78384dabc9
Bruno Rocha Dulcetti <brunodulcetti@gmail.com>
2026-03-30 22:17:47 -0300

    feat: add initial content for category page

--- app/category/[slug]/page.tsx
+++ app/category/[slug]/page.tsx
@@ -2,8 +2,10 @@ import { getAllPosts, getPostsByCategory } from '@/libs/posts';
 import { paginate } from '@/libs/pagination';
 import { Pagination } from '@/components/pagination';
 import { NewsHeroCard } from '@/components/news-hero-card';
+import { CATEGORIES_INFOS } from '@/libs/categories';
 
 import stylesHome from '@/styles/Home.module.scss';
+import stylesPost from '@/styles/Post.module.scss';
 
 type Props = {
   params: {
@@ -38,9 +40,19 @@ export default async function CategoriesPage({ params }: Props) {
   const { slug } = await params;
   const posts = await getPostsByCategory(slug);
   const { data, totalPages } = paginate(posts, 1, POSTS_PER_PAGE);
+  const category = CATEGORIES_INFOS.filter((cat) => cat.slug === slug)
 
   return (
     <div className={stylesHome['content-home']}>
+      {category && (
+        <div className={stylesPost["cover-image-container"]}>
+          <img src={category[0].coverImage} alt={`Capa da categoria ${category[0].name}`} className={stylesPost['cover-image']} />
+        </div>
+      )}
+
+      <h1>{category[0].name}</h1>
+      <p>{category[0].description}</p>
+
       <ul className={stylesHome['list-posts']}>
         {data.map((post) => (
           <li key={post.slug} className={stylesHome['post']}>
