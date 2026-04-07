307118a276fce16583c100ae12f84028efb033fb
Bruno Rocha Dulcetti <brunodulcetti@gmail.com>
2026-04-07 11:08:47 -0300

    feat: add initial not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+import styles from '@/styles/NotFound.module.scss';
+import form from '@/styles/Form.module.scss';
+import Link from 'next/link';
+
+export const metadata = {
+  title: '404 - Página Não Encontrada',
+  description: 'A página que você está procurando não existe.',
+};
+
